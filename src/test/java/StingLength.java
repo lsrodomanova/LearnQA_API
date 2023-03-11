3726643b0e531a8c52c4cecbf4f1f7ec9021b388
macbook <lrodomanova81@gmail.com>
2023-03-11 19:04:55 +0300

    new test

---
+++ src/test/java/StingLength.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StingLength {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"qwerty", "", "qwertyuiopasdfgh"})
+    public void checkStringLength(String text) {
+        int number =  text.length();
+        assertTrue(number>=15);
+    }
+}
